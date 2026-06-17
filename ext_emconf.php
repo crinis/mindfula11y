73588263e75463573745e72d67579f391c633831
Felix Spittel <felix@mindfulmarkup.de>
2026-06-17 23:01:47 +0200

    chore: update extension metadata and TYPO3 constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,12 +6,12 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'module',
     'author' => 'Felix Spittel',
     'author_email' => 'felix@mindfulmarkup.de',
-    'author_company' => 'Websites für Alle',
+    'author_company' => 'Mindful Markup',
     'state' => 'beta',
     'version' => '0.10.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-14.99.99',
+            'typo3' => '13.4.0-14.3.99',
             'php' => '8.2.0-8.4.99',
         ],
         'conflicts' => [],
